b8bd3037b0f66477c28022a60d8b3046a72b9966
Bharti0294 <bharti.nehul07@gmail.com>
2021-03-27 23:05:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
+import { Section1Component } from './section1/section1.component';
+import { ServiceComponent } from './service/service.component';
+import { ClientsectionComponent } from './clientsection/clientsection.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { TeamsectionComponent } from './teamsection/teamsection.component';
+import { ContactsectionComponent } from './contactsection/contactsection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    Section1Component,
+    ServiceComponent,
+    ClientsectionComponent,
+    PortfolioComponent,
+    TestimonialComponent,
+    TeamsectionComponent,
+    ContactsectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clientsection/clientsection.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientsectionComponent } from './clientsection.component';
+
+describe('ClientsectionComponent', () => {
+  let component: ClientsectionComponent;
+  let fixture: ComponentFixture<ClientsectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClientsectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientsectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
